9efd812614d115efd65651d8bd354d78f7a84c2c
Ashley Follen <ashleyfollen@outlook.com>
2021-07-20 19:26:47 -0600

    commit tree 85ae23890d12e8633c6244335aef2cb0f75be2f8

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+//function mondayWork(activity = "go to the office") {
+//    return `This Monday, I will ${activity}.`
+//}
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+//function wrapAdjective(param = "*") {
+//    return function(adj = "special") {
+//        return `You are ${param}${adj}${param}!`
+//    }
+//}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = 'special') {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+
+const encouragingPromptFunction = wrapAdjective('%')('a dedicated programmer')
